d546f0e54157c705c8311a62793ae10eab646b3a
halubko <misha.golubko@gmail.com>
2025-12-08 20:25:52 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+   tseslint.configs.recommended,
+   pluginVue.configs["flat/essential"],
+   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+   eslintPluginPrettierRecommended,
+   eslintConfigPrettier,
+   {
+      files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      languageOptions: { globals: globals.node },
+      rules: {
+         "prettier/prettier": "warn",
+         "no-console": "warn",
+      },
+   },
+]);
