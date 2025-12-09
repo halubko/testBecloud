a2e6aebaa4224206f1873b48d669a1fd59cd481a
halubko <misha.golubko@gmail.com>
2025-12-09 21:56:43 +0300

    added router queries filtering

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -1,24 +1,49 @@
 <script setup lang="ts">
 import { useGetUsers } from "@/composables/useGetUsers";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import UserCard from "@/components/users/UserCard.vue";
+import { useRoute } from "vue-router";
 
 const page = ref(1);
+const route = useRoute();
 
-const { data } = useGetUsers({ page });
+const filtersComputed = computed(() => {
+   return {
+      minAge: route.query.minAge ? Number(route.query.minAge) : undefined,
+      maxAge: route.query.maxAge ? Number(route.query.maxAge) : undefined,
+      gender: route.query.gender && (route.query.gender as string),
+   };
+});
+
+const { data, isLoading } = useGetUsers({ page, filters: filtersComputed });
 </script>
 
 <template>
-   <v-container>
-      <v-row>
-         <v-col v-for="user in data?.results" :key="user.id.value" sm="6" md="4" lg="3">
-            <user-card :user="user" />
-         </v-col>
+   <v-container class="d-flex flex-column justify-space-between h-100" fluid>
+      <v-row dense>
+         <template v-if="isLoading">
+            <v-col v-for="n in 12" :key="n" cols="12" sm="6" md="4" lg="3">
+               <v-skeleton-loader type="card-avatar, actions" />
+            </v-col>
+         </template>
+
+         <template v-else>
+            <v-col
+               v-for="user in data?.results"
+               :key="user.id.value"
+               cols="12"
+               sm="6"
+               md="4"
+               lg="3"
+            >
+               <user-card :user="user" />
+            </v-col>
+         </template>
       </v-row>
 
-      <v-row justify="center" class="mt-4">
+      <v-row justify="center" class="mt-4 flex-grow-0">
          <v-col cols="auto">
-            <v-pagination v-model="page" :length="10"></v-pagination>
+            <v-pagination v-model="page" :length="10" density="compact" />
          </v-col>
       </v-row>
    </v-container>
--- src/composables/useGetUsers.ts
+++ src/composables/useGetUsers.ts
@@ -1,10 +1,34 @@
 import { useQuery } from "@tanstack/vue-query";
 import { getUsers } from "../api/userAPI.ts";
-import { type MaybeRef, toValue } from "vue";
+import { type ComputedRef, type MaybeRef, toValue } from "vue";
+import type { UserFilterInterface } from "@/interfaces/User.Interfaces.ts";
 
-export const useGetUsers = ({ page }: { page: MaybeRef<number> }) => {
+interface useGetUsersProps {
+   page: MaybeRef<number>;
+   filters: ComputedRef<UserFilterInterface>;
+}
+
+export const useGetUsers = ({ page, filters }: useGetUsersProps) => {
    return useQuery({
-      queryKey: ["users", page],
-      queryFn: () => getUsers(toValue(page), 12),
+      queryKey: ["users", page, filters],
+      queryFn: () => {
+         const filter = toValue(filters);
+         return getUsers(toValue(page), 12, filter.gender);
+      },
+      select: (data) => {
+         const filter = toValue(filters);
+         let users = data.results;
+
+         if (filter.minAge) {
+            users = users.filter((user) => user.registered.age > filter.minAge!);
+            data.results = users;
+         }
+         if (filter.maxAge) {
+            users = users.filter((user) => user.registered.age < filter.maxAge!);
+            data.results = users;
+         }
+
+         return data;
+      },
    });
 };
