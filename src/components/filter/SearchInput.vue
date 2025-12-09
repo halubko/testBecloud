8f0b761371bc6791b6f4850e36bb41eed3e8e6f1
halubko <misha.golubko@gmail.com>
2025-12-10 02:18:23 +0300

    added search input

---
+++ src/components/filter/SearchInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { ref, watch } from "vue";
+
+const router = useRouter();
+
+const inputValue = ref();
+
+watch(
+   () => inputValue.value,
+   (value) => {
+      router.push({
+         query: {
+            search: value === "" ? undefined : value,
+         },
+      });
+   }
+);
+</script>
+
+<template>
+   <v-text-field
+      rounded="lg"
+      prepend-inner-icon="mdi-magnify"
+      clearable
+      placeholder="Search by name"
+      variant="solo"
+      v-model="inputValue"
+      max-width="600"
+   />
+</template>
