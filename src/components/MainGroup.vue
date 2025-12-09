84e261e8f01735f7341f0a16fcf3830d321c2fa9
halubko <misha.golubko@gmail.com>
2025-12-10 02:22:08 +0300

    ui fix

--- src/components/MainGroup.vue
+++ src/components/MainGroup.vue
@@ -1,11 +1,19 @@
 <template>
-   <FilterGroup />
    <v-main>
-      <UserList />
+      <v-container class="d-flex flex-column justify-space-between h-100">
+         <div>
+            <SearchInput />
+            <UserList :page="page" />
+         </div>
+         <v-pagination v-model="page" :length="10" />
+      </v-container>
    </v-main>
 </template>
 
 <script setup lang="ts">
-import UserList from "@/components/users/UserList.vue";
-import FilterGroup from "@/components/filter/FilterGroup.vue";
+import UserList from "@/components/users/UsersList.vue";
+import { ref } from "vue";
+import SearchInput from "@/components/filter/SearchInput.vue";
+
+const page = ref(1);
 </script>
