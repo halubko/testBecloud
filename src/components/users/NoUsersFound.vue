a302eef6d1853af384f2097b8f3d9385f91a9dfe
halubko <misha.golubko@gmail.com>
2025-12-10 02:15:30 +0300

    added no users found card

---
+++ src/components/users/NoUsersFound.vue
@@ -0,0 +1,7 @@
+<template>
+   <v-card rounded="lg" class="d-flex flex-column align-center pa-4">
+      <v-icon icon="mdi-alert-circle-outline" color="red" size="64" />
+      <v-card-title class="text-h5 font-weight-bold"> No users found </v-card-title>
+   </v-card>
+</template>
+<script setup lang="ts"></script>
