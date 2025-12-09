86825b74a1042f60e07f0d8f0c45626c9ff667e2
halubko <misha.golubko@gmail.com>
2025-12-09 21:57:46 +0300

    updated api for gender param

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -6,12 +6,18 @@ const axiosInstance = axios.create({
    baseURL: BASE_URL,
 });
 
-export const getUsers = async (page: number, results: number) => {
+export const getUsers = async (page: number, results: number, gender?: "male" | "female") => {
+   const params: Record<string, number | string> = {
+      page,
+      results,
+   };
+
+   if (gender) {
+      params.gender = gender;
+   }
+
    const response: AxiosResponse<UserResponseInterfaces> = await axiosInstance.get("", {
-      params: {
-         page,
-         results,
-      },
+      params,
    });
    return response.data;
 };
