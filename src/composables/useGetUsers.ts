b5e3488a62bb89c450cfcf9726df42a13ea4f813
halubko <misha.golubko@gmail.com>
2025-12-10 02:17:15 +0300

    added filtration on a data

--- src/composables/useGetUsers.ts
+++ src/composables/useGetUsers.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/vue-query";
 import { getUsers } from "../api/userAPI.ts";
-import { type MaybeRef, toValue } from "vue";
+import { computed, type MaybeRef, toValue } from "vue";
 import type { UserFilterInterface } from "@/interfaces/User.Interfaces.ts";
+import { formatUserName } from "@/utils/helpers.ts";
 
 interface useGetUsersProps {
    page: MaybeRef<number>;
@@ -9,26 +10,50 @@ interface useGetUsersProps {
 }
 
 export const useGetUsers = ({ page, filters }: useGetUsersProps) => {
-   return useQuery({
-      queryKey: ["users", page, filters],
+   const query = useQuery({
+      queryKey: ["users", page, () => computed(() => toValue(filters).gender)],
       queryFn: () => {
          const filter = toValue(filters);
          return getUsers(toValue(page), 12, filter.gender);
       },
-      select: (data) => {
-         const filter = toValue(filters);
-         let users = data.results;
-
-         if (filter.minAge) {
-            users = users.filter((user) => user.registered.age > filter.minAge!);
-            data.results = users;
-         }
-         if (filter.maxAge) {
-            users = users.filter((user) => user.registered.age < filter.maxAge!);
-            data.results = users;
-         }
-
-         return data;
-      },
    });
+
+   const filteredUsers = computed(() => {
+      const data = query.data.value;
+      const filter = toValue(filters);
+
+      if (!data)
+         return {
+            results: [],
+            info: {
+               results: 0,
+               page: 0,
+            },
+         };
+
+      let users = data.results;
+      if (filter.minAge) {
+         users = users.filter((user) => user.registered.age >= filter.minAge!);
+      }
+      if (filter.maxAge) {
+         users = users.filter((user) => user.registered.age <= filter.maxAge!);
+      }
+      if (filter.search) {
+         users = users.filter((user) =>
+            formatUserName(user.name.first, user.name.last)
+               .toLowerCase()
+               .includes(filter.search!.toLowerCase())
+         );
+      }
+
+      return {
+         ...data,
+         results: users,
+      };
+   });
+
+   return {
+      isLoading: query.isLoading,
+      data: filteredUsers,
+   };
 };
