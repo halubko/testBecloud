b5db60f4bd57514f883fb7ffe5132935d7bd95c5
halubko <misha.golubko@gmail.com>
2025-12-10 02:22:14 +0300

    ui fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import MainGroup from "@/components/MainGroup.vue";
+import FilterGroup from "@/components/filter/FilterGroup.vue";
 </script>
 
 <template>
    <v-app>
+      <FilterGroup />
       <MainGroup />
    </v-app>
 </template>
