ad1490a4491bee9cc6f8cb92fe651a69f4e8d287
halubko <misha.golubko@gmail.com>
2025-12-10 11:12:48 +0300

    uninstall pinia

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { createPinia } from "pinia";
 import vuetify from "./plugins/vuetify.ts";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import router from "@/router";
 
-const pinia = createPinia();
 const app = createApp(App);
 
 app.use(VueQueryPlugin);
 app.use(router);
 app.use(vuetify);
-app.use(pinia);
 app.mount("#app");
